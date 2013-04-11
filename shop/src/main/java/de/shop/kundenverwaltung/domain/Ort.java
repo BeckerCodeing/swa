266cbe7b8b34a01c341ab41cd7c423c2b66c5ba6
Jan Krieghoff <krja1012(at)hs-karlsruhe.de>
2013-04-11 15:47:15 +0200

    Klasse Ort vervollständigt.

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Ort.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Ort.java
@@ -4,6 +4,8 @@ public class Ort {
 	
 	private int plz;
 	private String bezeichnung;
+	private String strasse;
+	private int hausnummer;
 	
 	public int getPlz() {
 		return plz;
@@ -18,15 +20,27 @@ public class Ort {
 		this.bezeichnung = bezeichnung;
 	}
 	
-	
-	// Overrides
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public int getHausnummer() {
+		return hausnummer;
+	}
+	public void setHausnummer(int hausnummer) {
+		this.hausnummer = hausnummer;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result
 				+ ((bezeichnung == null) ? 0 : bezeichnung.hashCode());
+		result = prime * result + hausnummer;
 		result = prime * result + plz;
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
 		return result;
 	}
 	
@@ -44,14 +58,22 @@ public class Ort {
 				return false;
 		} else if (!bezeichnung.equals(other.bezeichnung))
 			return false;
+		if (hausnummer != other.hausnummer)
+			return false;
 		if (plz != other.plz)
+			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
 			return false;
 		return true;
 	}
 	
 	@Override
 	public String toString() {
-		return "Ort [plz=" + plz + ", bezeichnung=" + bezeichnung + "]";
+		return "Ort [plz=" + plz + ", bezeichnung=" + bezeichnung
+				+ ", strasse=" + strasse + ", hausnummer=" + hausnummer + "]";
 	}
 	
 
