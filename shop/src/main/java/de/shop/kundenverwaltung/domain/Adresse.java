0dbec21833cb944736ae89c4f505b63f93cb952e
Jan Krieghoff <krja1012(at)hs-karlsruhe.de>
2013-04-11 15:40:27 +0200

    Klasse Adresse mit Overrides

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -1,5 +1,55 @@
 package de.shop.kundenverwaltung.domain;
 
 public class Adresse {
-//TODO: Logik für die Adresse erstellen!
+	
+	private Long id;
+	private Ort ort;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Ort getOrt() {
+		return ort;
+	}
+	public void setOrt(Ort ort) {
+		this.ort = ort;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adresse other = (Adresse) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (ort == null) {
+			if (other.ort != null)
+				return false;
+		} else if (!ort.equals(other.ort))
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "Adresse [id=" + id + ", ort=" + ort + "]";
+	}
 }
