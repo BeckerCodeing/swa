54b8aa8d491478abc866c2d8676bc7628b33f9e6
Britta Boldt <bobr1012 (at) hs-karlsruhe.de>
2013-04-14 19:23:59 +0200

    Bestellung, BestellungResource, UriHelperBestellung erstellt und LocalHelper aus Beispiel übernommen.

--- shop/src/main/java/de/shop/bestellverwaltung/rest/UriHelperBestellung.java
+++ shop/src/main/java/de/shop/bestellverwaltung/rest/UriHelperBestellung.java
@@ -1,7 +1,37 @@
 package de.shop.bestellverwaltung.rest;
 
-public class UriHelperBestellung {
-//TODO: UriHelperBestellung > inhalt!
+import java.net.URI;
 
-// Musste erstellt werden da ich es für meine UriHelperKunde brauche!
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import de.shop.bestellverwaltung.domain.Bestellung;
+import de.shop.kundenverwaltung.domain.Kunde;
+import de.shop.kundenverwaltung.rest.UriHelperKunde;
+
+@ApplicationScoped
+public class UriHelperBestellung {
+
+	@Inject
+	private UriHelperKunde uriHelperKunde;
+	
+	public void updateUriBestellung(Bestellung bestellung, UriInfo uriInfo) {
+		// URL fuer Kunde setzen
+		final Kunde kunde = bestellung.getKunde();
+		if (kunde != null) {
+			final URI kundeUri = uriHelperKunde.getUriKunde(bestellung.getKunde(), uriInfo);
+			bestellung.setKundeUri(kundeUri);
+		}
+		
+	}
+
+	public URI getUriBestellung(Bestellung bestellung, UriInfo uriInfo) {
+		final UriBuilder ub = uriInfo.getBaseUriBuilder()
+		                             .path(BestellungResource.class)
+		                             .path(BestellungResource.class, "findBestellungById");
+		final URI uri = ub.build(bestellung.getId());
+		return uri;
+	}
 }
