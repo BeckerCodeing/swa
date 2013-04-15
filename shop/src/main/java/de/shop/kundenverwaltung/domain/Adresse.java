a3cbc4e7d203dc8e2cc6fa9ce5064c1206d72659
Jan Krieghoff <krja1012(at)hs-karlsruhe.de>
2013-04-15 15:51:41 +0200

    Behoben

--- shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
+++ shop/src/main/java/de/shop/kundenverwaltung/domain/Adresse.java
@@ -4,6 +4,8 @@ public class Adresse {
 	
 	private Long id;
 	private Ort ort;
+	private String strasse;
+	private int hausnummer;
 	
 	public Long getId() {
 		return id;
@@ -17,15 +19,26 @@ public class Adresse {
 	public void setOrt(Ort ort) {
 		this.ort = ort;
 	}
-	
-	
-	// Overrides
+	public String getStrasse() {
+		return strasse;
+	}
+	public void setStrasse(String strasse) {
+		this.strasse = strasse;
+	}
+	public int getHausnummer() {
+		return hausnummer;
+	}
+	public void setHausnummer(int hausnummer) {
+		this.hausnummer = hausnummer;
+	}
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + hausnummer;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		result = prime * result + ((ort == null) ? 0 : ort.hashCode());
+		result = prime * result + ((strasse == null) ? 0 : strasse.hashCode());
 		return result;
 	}
 	@Override
@@ -37,6 +50,8 @@ public class Adresse {
 		if (getClass() != obj.getClass())
 			return false;
 		Adresse other = (Adresse) obj;
+		if (hausnummer != other.hausnummer)
+			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -47,11 +62,17 @@ public class Adresse {
 				return false;
 		} else if (!ort.equals(other.ort))
 			return false;
+		if (strasse == null) {
+			if (other.strasse != null)
+				return false;
+		} else if (!strasse.equals(other.strasse))
+			return false;
 		return true;
 	}
-	
 	@Override
 	public String toString() {
-		return "Adresse [id=" + id + ", ort=" + ort + "]";
+		return "Adresse [id=" + id + ", ort=" + ort + ", strasse=" + strasse
+				+ ", hausnummer=" + hausnummer + "]";
 	}
+
 }
